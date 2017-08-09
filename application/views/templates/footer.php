f56c8ebb5c318980cdc1ec1a608ca3013dc99334
honza <honza@Home1B>
2017-08-09 14:35:34 -0700

    Init commit

---
+++ application/views/templates/footer.php
@@ -0,0 +1,49 @@
+
+                       <!-- Footer Start -->
+                       <div class="col-md-12 page-body margin-top-50 footer">
+                          <footer>
+                          <ul class="menu-link">
+                               <li><a href="index.html">Home</a></li>
+                               <li><a href="about.html">About</a></li>
+                               <li><a href="work.html">Work</a></li>
+                               <li><a href="contact.html">Contact</a></li>
+                            </ul>
+
+                          <p>&copy; Copyright 2016 - <?php echo Date('Y'); ?>  <?php bloginfo('name'); ?>. All rights reserved</p>
+
+
+						  <!-- UiPasta Credit Start -->
+                          <div class="uipasta-credit">Page design inspired by <a href="http://www.uipasta.com" target="_blank">UiPasta</a></div>
+                          <!-- UiPasta Credit End -->
+
+
+                         </footer>
+                         <?php wp_footer(); ?>
+                       </div>
+                       <!-- Footer End -->
+
+
+                  </div>
+                  <!-- Blog Post (Right Sidebar) End -->
+
+            </div>
+         </div>
+      </div>
+
+
+
+    <!-- Back to Top Start -->
+    <a href="#" class="scroll-to-top"><i class="fa fa-long-arrow-up"></i></a>
+    <!-- Back to Top End -->
+
+
+    <!-- All Javascript Plugins  -->
+    <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery.min.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/plugin.js"></script>
+
+    <!-- Main Javascript File  -->
+    <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/scripts.js"></script>
+
+
+   </body>
+ </html>
